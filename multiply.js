015c60a162c35634fa884a944b01f3a97b7578fd
sorftgithub <oluwasogoemma2017@gmail.com>
2026-04-05 12:16:53 -0700

    converting string to number

--- multiply.js
+++ multiply.js
@@ -3,14 +3,15 @@
 function multiply(a, b){
  return a * b
 }
+ 
+let multiply2 = (a, b) => a * b
 
-
-    console.log(multiply(1,1), 1);
-    console.log(multiply(2,1), 2);
-    console.log(multiply(2,2), 4);
-    console.log(multiply(3,5), 15); 
-    console.log(multiply(5,0), 0);
-    console.log(multiply(0,5), 0);
-    console.log(multiply(0,0), 0); 
+    console.log(multiply2(1,1), 1);
+    console.log(multiply2(2,1), 2);
+    console.log(multiply2(2,2), 4);
+    console.log(multiply2(3,5), 15); 
+    console.log(multiply2(5,0), 0);
+    console.log(multiply2(0,5), 0);
+    console.log(multiply2(0,0), 0); 
 
 
---
+++ string-number.js
@@ -0,0 +1,18 @@
+// We need a function that can transform a string into a number. 
+// What ways of achieving this do you know?
+
+
+// let stringToNumber = function(str) {
+//     return (Number(str))
+
+// }
+
+let stringToNumber = (str) => (Number (str))
+
+
+    console.log(stringToNumber("1234"),1234)
+    console.log(stringToNumber("605"), 605)
+    console.log(stringToNumber("1405"),1405)
+    console.log(stringToNumber("-7"),  -7)
+
+    
