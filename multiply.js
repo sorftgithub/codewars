666756519b714586a699e75dea00887bc8433248
sorftgithub <oluwasogoemma2017@gmail.com>
2026-04-05 09:23:12 -0700

    solved codewars challenge

---
+++ multiply.js
@@ -0,0 +1,16 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+ return a * b
+}
+
+
+    console.log(multiply(1,1), 1);
+    console.log(multiply(2,1), 2);
+    console.log(multiply(2,2), 4);
+    console.log(multiply(3,5), 15); 
+    console.log(multiply(5,0), 0);
+    console.log(multiply(0,5), 0);
+    console.log(multiply(0,0), 0); 
+
+
