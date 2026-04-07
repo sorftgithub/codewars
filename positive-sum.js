858b378223f5de0b5aba1977c6a8f5a064fad8fd
sorftgithub <oluwasogoemma2017@gmail.com>
2026-04-07 04:21:30 -0700

    add array of positive numbers only and vice-vers

--- positive-sum.js
+++ positive-sum.js
@@ -17,3 +17,21 @@ function positiveSum(arr){
     console.log(positiveSum([-1,-2,-3,-4,-5]),0);
     console.log(positiveSum([-1,2,3,4,-5]),9);
    
+// You get an array of numbers, return the sum of all of the positives ones.
+
+    function negativeSum(arr) {
+        let total = 0;
+        for (let num of arr){
+            if(num < 0){
+                total += num;
+            }
+        }
+        return total;
+    }
+
+
+    console.log(negativeSum([1,2,3,4,5]),0);
+    console.log(negativeSum([1,-2,3,4,5]),-2);
+    console.log(negativeSum([]),0);
+    console.log(negativeSum([-1,-2,-3,-4,-5]),-15);
+    console.log(negativeSum([-1,2,3,4,-5]),-6);
