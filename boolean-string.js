b5435a38287a8d6acf0a8a0f5422cd59dd31c8ac
sorftgithub <oluwasogoemma2017@gmail.com>
2026-04-07 03:21:23 -0700

    converting boolean to string

---
+++ boolean-string.js
@@ -0,0 +1,16 @@
+// Complete the method that takes a boolean value and return
+//  a "Yes" string for true, or a "No" string for false.
+
+
+// function boolToWord(bool){
+//     if (bool === true){
+//         return "Yes"
+//     } else{
+//         return "No"
+//     }
+// }
+
+let boolToWord = (bool) => bool === true ? "Yes" : "No"
+
+console.log(boolToWord(true), "Yes" )
+console.log(boolToWord(false), "No")
