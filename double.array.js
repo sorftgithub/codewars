3d357ac58e678cde296e87a473a669ca099aa573
sorftgithub <oluwasogoemma2017@gmail.com>
2026-04-18 03:51:00 -0700

    diuble the numbers in an array

---
+++ double.array.js
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+
+function maps(x){
+    return x.map(n => n * 2)
+}
+
+
+
+    console.log(maps([1, 2, 3]), [2, 4, 6]);
+    console.log(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]); 
+    console.log(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]); 
