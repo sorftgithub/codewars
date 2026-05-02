4c1d50d539a4fc2a7f154c309aacf42788e9169c
sorftgithub <oluwasogoemma2017@gmail.com>
2026-05-02 04:28:51 -0700

    returning string

---
+++ returning-str.js
@@ -0,0 +1,9 @@
+// Create a function that accepts a parameter representing a
+// name and returns the message: "Hello, <name> how are you doing today?".
+
+function greet(name){
+    return `Hello, ${name} how are ypu doing today?`
+}
+
+    console.log(greet("Ryan"), "Hello, Ryan how are you doing today?");
+    console.log(greet("Shingles"),  "Hello, Shingles how are you doing today?");
