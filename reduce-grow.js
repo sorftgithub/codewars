05bef78e7b923204fe7e2565907cb4e013959acb
sorftgithub <oluwasogoemma2017@gmail.com>
2026-05-12 01:35:29 -0700

    multiplying all the num in an array together

---
+++ reduce-grow.js
@@ -0,0 +1,9 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order.
+
+function grow(x){
+    return x.reduce((a,b) => a * b, 1)
+}
+
+    console.log(grow([1, 2, 3]), 6);
+    console.log(grow([4, 1, 1, 1, 4]), 16); 
+    console.log(grow([2, 2, 2, 2, 2, 2]), 64); 
