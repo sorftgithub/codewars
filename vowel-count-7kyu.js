3845d75878a1a1a8e0d80bc83e25d80eb1eb13ec
sorftgithub <137712425+sorftgithub@users.noreply.github.com>
2026-06-03 18:06:32 -0700

    count the num of vowels in a str

---
+++ vowel-count-7kyu.js
@@ -0,0 +1,19 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str){
+    let vowelCounts = 0;
+    const vowels = "aeiou";
+    for(let char of str){
+        if(vowels.includes(char)){
+            vowelCounts++
+        }
+    }
+    return vowelCounts
+}
+
+
+console.log(getCount("abracadabra"), 5)
