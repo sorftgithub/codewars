1abb7ce76e9db25a11ed3d0d576e025fcfd5d993
sorftgithub <137712425+sorftgithub@users.noreply.github.com>
2026-06-24 10:18:51 -0700

    The greeting is personalised if the nme agrees with the owner

---
+++ personal-msg.js
@@ -0,0 +1,13 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+let greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest"
+
+
+console.log(greet('Daniel', 'Daniel'), 'Hello boss')
+console.log(greet('Greg', 'Daniel'), 'Hello guest')
